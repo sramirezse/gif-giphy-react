5d463d981b9dcdc539968588e812f226a70daa90
gerardo Ramirez <sramirezse@gmail.com>
2021-10-17 10:21:13 -0500

    m

---
+++ src/GifExpertApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GifExpertApp = () => {
+    return (
+        <div>
+            <h2> GifExpertAppJS </h2>
+            <hr></hr>
+        </div>
+    )
+}
